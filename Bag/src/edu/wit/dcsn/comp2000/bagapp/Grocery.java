928698b11a4ecdfa2d5c3e8c13cfbe8491430b5e
Pedro Moter <pedromoter@me.com>
2020-02-05 19:35:26 -0500

    Added interpretations and to strings

--- Bag/src/edu/wit/dcsn/comp2000/bagapp/Grocery.java
+++ Bag/src/edu/wit/dcsn/comp2000/bagapp/Grocery.java
@@ -4,12 +4,49 @@ public class Grocery {
 
 		private String name;
 		private GroceryItemSize size;
+		private GroceryItemBreakability breakability;
+		private GroceryItemMaterial material;
+		private GroceryItemPerishability perishability;
+		private GroceryItemRigidness rigdness;
+		private GroceryItemWeight weight;
 		
 		
-	   @Override
-		public String toString() {
-			
-	   System.out.println(name);
-	    return name + " size: " + size;
+		public Grocery(String name, String size, String breakability,
+				String material, String perishability, String rigdness,
+				String weight) {
+			super();
+			this.name = name;
+			this.size = GroceryItemSize.interpretDescription(size);
+			this.breakability = GroceryItemBreakability.interpretDescription(breakability);
+			this.material = GroceryItemMaterial.interpretDescription(material);
+			this.perishability = GroceryItemPerishability.interpretDescription(perishability);
+			this.rigdness = GroceryItemRigidness.interpretDescription(rigdness);
+			this.weight = GroceryItemWeight.interpretDescription(weight);
 		}
+
+		
+		public boolean compareTo( Grocery item) {
+			
+			if(item.breakability.sizeValue > this.breakability.sizeValue) {
+				return false;	
+			}
+		
+			
+			
+			return true;
+		}
+
+		@Override
+		public String toString() {
+			return "Grocery [name=" + name + ", size=" + size + ", breakability=" + breakability + ", material="
+					+ material + ", perishability=" + perishability + ", rigdness=" + rigdness + ", weight=" + weight
+					+ "]";
+		}
+		
+		
+
+
+
+		
+		
 }
--- Bag/src/edu/wit/dcsn/comp2000/bagapp/GroceryBagger.java
+++ Bag/src/edu/wit/dcsn/comp2000/bagapp/GroceryBagger.java
@@ -6,11 +6,20 @@ import java.util.Scanner;
 
 public class GroceryBagger {
 	
-	
+		private static ArrayBag
+		
+		private static int numberOfItems = 0;
+		
 		public static void main(String[] args) {
 			
 			//read from file here 
 		    File file = new File("data/groceries.txt");
+
+			// initialize number of bags = number of groceries (simulates unlimited number of bags, some remain empty at the end)
+			// I assume we need to make a Bag class and implement the BagInterface interface
+//			Bag[] bag = new Bag[ numberOfBags ]; // array of Bags
+			
+			
 
 		    
 		    try {
@@ -29,24 +38,37 @@ public class GroceryBagger {
 		        System.out.println("File was not found, please retry!");
 		    }
 		    catch(Exception e ){
-		        System.out.println("Unkown error ocurred, please retry!");
+		        System.out.println("Unkown error ocurred. "+ e.getMessage());
 
 		    }
-			
-	
-			
-			
 			
 			
 		}	
 		
+				
 		private static void interpretDescription(String entry) {
-			
-
+			numberOfItems++;
 			String[] data = entry.split("\t");
+			Grocery grocery = new Grocery(data[0], 
+					data[1],
+					data[5],
+					data[3], 
+					data[6], 
+					data[4],
+					data[2]);
+			
+			addToBag(grocery);
+			
+		}
+		
+		private static void addToBag(Grocery grocery) {
 			
 			
 			
+			
+		}
+		
+		private static String displayData() {
 			
 		}
 	
